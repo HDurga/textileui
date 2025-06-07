57891c2c7a81bace453f4591d23f46a7adb1bba1
HDurga <durgakalaparthi@gmail.com>
2025-06-07 16:59:25 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import OrganicProductsPage from './pages/Organicproducts/homeorganic';
-import AboutUsPage from './pages/AboutUsPage/AboutUsPage'; // Import your new page
+import AboutUsPage from './pages/AboutUsPage/AboutUsPage'; 
+
 
 function App() {
   return (
